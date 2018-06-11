da377802fa44ccf3b16efd47b943d9ebf2935d3e
Danyaal <danyaalsoni@gmail.com>
2018-06-10 20:11:12 -0400

    Add project files.

---
+++ WpfApp1/Database/RentalAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WpfApp1.Database
+{
+    [Table("RentalAddress")]
+    class RentalAddress
+    {
+        public int ID { get; set; }
+        public string address { get; set; }
+        public int renterID { get; set; }
+    }
+}
---
+++ WpfApp1/Database/Renter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WpfApp1
+{
+    [Table("Renter")]
+    public class Renter
+    {
+        public int ID { get; set; }
+        public string tenantName { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public double rent { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime termEnd { get; set; }
+        public double deposit { get; set; }
+        public double cleaningDeposit { get; set; }
+        public double keyDeposit { get; set; }
+
+    }
+}
